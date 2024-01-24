1a2c821e787618de3b9d32ea8620775948aa67a7
0Clark30 <austinlaw.clark@outlook.com>
2024-01-23 18:09:51 -0600

    added initial file structure, class layout, test example comment

---
+++ Assets/lib/shapes.js
@@ -0,0 +1,17 @@
+class Shape {
+   constructor() {} 
+}
+
+class Circle extends Shape {
+  constructor() {}
+}
+
+class Triangle extends Shape {
+  constructor() {}
+}
+
+class Square extends shape {
+  constructor() {}
+}
+
+
