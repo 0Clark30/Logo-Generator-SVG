5786adabe60ff121fc2fc7cefe02a93539385bb4
0Clark30 <austinlaw.clark@outlook.com>
2024-01-23 18:26:08 -0600

    added shape svg elements to render function in shape classes (in progress)

--- Assets/lib/shapes.js
+++ Assets/lib/shapes.js
@@ -1,17 +1,32 @@
 class Shape {
-   constructor() {} 
+   constructor(color) {
+    this.color = color;
+   } 
 }
 
 class Circle extends Shape {
-  constructor() {}
+  constructor() {
+    render () {
+       `<circle cx="150 cy="100 r="80" fill= "${shape.color}"`
+    }
+  }
 }
 
 class Triangle extends Shape {
-  constructor() {}
+  constructor() {
+    render(){
+  `<polygon points="150, 18 244, 182 56, 182" fill="${shape.color}" />`
+}
+  }
 }
 
+//need to add polygon points to match a square
 class Square extends shape {
-  constructor() {}
+  constructor() {
+     render(){
+  `<polygon points="150, 18 244, 182 56, 182" fill="${shape.color}" />`
+}
+  }
 }
 
 
